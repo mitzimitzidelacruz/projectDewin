3e386338ae09a5ff84d6092d452aa1425358082f
vero <veronica.delacruz12@hotmail.com>
2022-10-01 21:58:58 -0500

    Add Footer and careers page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import {RegisterComponent} from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { CareersComponent } from './careers/careers.component';
 
 const routes: Routes = [
 
 {path: 'login', component: LoginComponent},
 {path: 'register', component: RegisterComponent},
 {path: 'home', component: HomeComponent},
+{path: 'careers', component: CareersComponent},
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CareersComponent } from './careers/careers.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { NavbarComponent } from './navbar/navbar.component';
     LoginComponent,
     RegisterComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    CareersComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
