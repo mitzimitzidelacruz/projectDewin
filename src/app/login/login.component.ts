78891b8f6176844522f827268185192ad7225670
vero <veronica.delacruz12@hotmail.com>
2022-10-09 22:32:20 -0500

    Add test validations

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,7 +11,20 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
   }
   
+  test (){
 
+    //validaciones que creo que no esta bien
+    let inputPassword = document.getElementById("password") as HTMLInputElement;
+    let valuesPassword = inputPassword.value;
+    if (valuesPassword.length >= 8) {
+      console.log("Ingreso correctamente");
+    }else{
+      alert("El Minimo de Caracteres es de 8");
+    }
+  }
+  
 }
+
